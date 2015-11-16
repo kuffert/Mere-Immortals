a24f3a211caccb603859a3ee97197811b2540284
Chris Kuffert <chriskuffert@comcast.net>
2015-11-15 22:23:58 -0500

    Added some weather functionality, switching platforms to continue working

---
+++ Assets/NonAttached Scripts/Weather.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class Weather {
+
+    public static Weather weather;
+    public Sprite noWeatherFoundSprite;
+    List<WeatherVectorSpritePair> weatherEffects = new List<WeatherVectorSpritePair>();
+
+
+
+    public Sprite findSpriteByWeatherVector(Vector2 weatherVector)
+    {
+        foreach (WeatherVectorSpritePair pair in weatherEffects) {
+
+            if (pair.getVectorEffect().Equals(weatherVector)) {
+                return pair.getWeatherSprite();
+            }
+         }
+
+        // If it isn't found, something went wrong.
+        Debug.Log("No weather effect found for vector: " + weatherVector);
+        return noWeatherFoundSprite;
+    }
+
+    class WeatherVectorSpritePair {
+
+        Vector2 vectorEffect;
+        Sprite weatherSprite;
+
+        WeatherVectorSpritePair(Vector2 ve, Sprite ws)
+        {
+            this.vectorEffect = ve;
+            this.weatherSprite = ws;
+        }
+
+        public Vector2 getVectorEffect() { return vectorEffect; }
+        public Sprite getWeatherSprite() { return weatherSprite; }
+    }
+}
+
+
+
+
+
