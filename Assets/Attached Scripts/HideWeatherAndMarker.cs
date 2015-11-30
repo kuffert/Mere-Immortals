48e149d50c3c67acf707754ac1b15ff8e5ef5be7
Matthew Di Mare Miller <millerdimarem@gmail.com>
2015-11-29 21:51:46 -0500

    town and show/hide weather map
    put the town in the background and made the weather map show/hide button

---
+++ Assets/Attached Scripts/HideWeatherAndMarker.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HideWeatherAndMarker : MonoBehaviour {
+
+	public GameObject button;
+	public GameObject weatherboard;
+	public GameObject marker;
+	private static int clicks = 0;
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	void onClick(){
+		print ("CLICKED!");
+		Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+		RaycastHit hit;
+		Physics.Raycast (ray, out hit);
+		if (button.GetComponent<BoxCollider> ().Raycast (ray, out hit, 100)) {
+
+			if(clicks == 0) {
+				clicks++;
+				weatherboard.GetComponent<Renderer>().enabled = false;
+				marker.GetComponent<Renderer>().enabled = false;
+			}
+			else{
+				clicks = 0;
+				weatherboard.GetComponent<Renderer>().enabled = true;
+				marker.GetComponent<Renderer>().enabled = true;
+			}
+		}
+	}
+
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown(0)) {
+			onClick ();
+		}
+	}
+
+}
